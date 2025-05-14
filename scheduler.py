6a4e88d28352ecafba85cdd51144e863cac88e56
mailsender123 <languagetranslator20@gmail.com>
2025-05-14 06:57:09 +0530

    scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,49 +1,43 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-import urllib.parse
 import sqlite3
+from datetime import datetime
+import time
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import os
 
-def schedule_email(to_email, subject, message, send_time):
-    conn = sqlite3.connect('emails.db')
-    c = conn.cursor()
-    c.execute('''
-        INSERT INTO scheduled_emails (to_email, subject, message, send_time)
-        VALUES (?, ?, ?, ?)
-    ''', (to_email, subject, message, send_time))
-    conn.commit()
-    conn.close()
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 
-class EmailSchedulerHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            with open('templates/index.html', 'rb') as file:
-                self.wfile.write(file.read())
-        else:
-            super().do_GET()
+def send_email(to_email, subject, message):
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        email_message = Mail(
+            from_email=SENDER_EMAIL,
+            to_emails=to_email,
+            subject=subject,
+            plain_text_content=message
+        )
+        response = sg.send(email_message)
+        print(f"Status Code: {response.status_code}")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode()
-        data = urllib.parse.parse_qs(post_data)
+def email_scheduler():
+    while True:
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        conn = sqlite3.connect('emails.db')
+        c = conn.cursor()
+        c.execute('''
+            SELECT id, to_email, subject, message FROM scheduled_emails 
+            WHERE send_time <= ?
+        ''', (now,))
+        emails = c.fetchall()
+        for email in emails:
+            send_email(email[1], email[2], email[3])
+            c.execute('DELETE FROM scheduled_emails WHERE id = ?', (email[0],))
+            conn.commit()
+        conn.close()
+        time.sleep(60)
 
-        to_email = data['to_email'][0]
-        subject = data['subject'][0]
-        message = data['message'][0]
-        send_time = data['send_time'][0]
-
-        schedule_email(to_email, subject, message, send_time)
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'Email scheduled successfully!')
-
-def run_server():
-    server_address = ('0.0.0.0', 8080)
-    httpd = HTTPServer(server_address, EmailSchedulerHandler)
-    print("Web server running on port 8080...")
-    httpd.serve_forever()
-
-run_server()
+print("Starting scheduler...")
+email_scheduler()
